bf6e11fb6e175947cdbfd4d76786ac5ea5d26b5e
m2rsh <mariuszbubak10@gmail.com>
2024-08-02 16:23:50 +0200

    Formating and simplified CreateReply

--- src/commands/iq.rs
+++ src/commands/iq.rs
@@ -3,43 +3,43 @@ use poise::{serenity_prelude::{self as serenity, CreateEmbed}, CreateReply};
 use rand_distr::{Distribution, Normal};
 
 /// IQ fun tingy something idk
-#[poise::command(slash_command, user_cooldown=3, rename="iq", category = "Fun")]
+#[poise::command(slash_command, user_cooldown = 3, rename = "iq", category = "Fun")]
 pub async fn run(
     ctx: Context<'_>,
     #[description = "Person"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
     let user = user.as_ref().unwrap_or_else(|| ctx.author());
 
-    let iq: f64 = 
-        if CONFIG.rig_ids.contains(&user.id.get()) {
-            Normal::new(100000000.0, 10000000.0).unwrap()
-                .sample(&mut rand::thread_rng())
-        } else {
-            Normal::new(100.0, 25.0).unwrap()
-                .sample(&mut rand::thread_rng())
-        };
-    
+    let iq: f64 = if CONFIG.rig_ids.contains(&user.id.get()) {
+        Normal::new(100000000.0, 10000000.0)
+            .unwrap()
+            .sample(&mut rand::thread_rng())
+    } else {
+        Normal::new(100.0, 25.0)
+            .unwrap()
+            .sample(&mut rand::thread_rng())
+    };
+
     // IQ can go over 24 but it will just cause the colour to be brighter untill every number is 255 or over it
     // which will just convert it to u8 and max it  o 255
-    let colour_thingy: f64 = iq/140.0; 
+    let colour_thingy: f64 = iq / 140.0;
     let colour = serenity::Colour::from_rgb(
-        (235.0*colour_thingy) as u8, 
-        (188.0*colour_thingy) as u8, 
-        (186.0*colour_thingy) as u8
+        (235.0 * colour_thingy) as u8,
+        (188.0 * colour_thingy) as u8,
+        (186.0 * colour_thingy) as u8,
     );
-    let emoji = if iq >= 110.0 { &CONFIG.emojis.bigbrain } else if 90.0 >= iq { &CONFIG.emojis.nobrain } else { &CONFIG.emojis.justbrain };
+    let emoji = if iq >= 110.0 {
+        &CONFIG.emojis.bigbrain
+    } else if 90.0 >= iq {
+        &CONFIG.emojis.nobrain
+    } else {
+        &CONFIG.emojis.justbrain
+    };
 
     let embed = CreateEmbed::default()
-        .description(format!(
-        "{} {} IQ is {:.2}",
-        emoji,
-        user,
-        iq,
-    ))
+        .description(format!("{} {} IQ is {:.2}", emoji, user, iq,))
         .colour(colour);
-     
-    ctx.send(
-        CreateReply{embeds: vec![embed], ..Default::default()}
-    ).await?;
+
+    ctx.send(CreateReply::default().embed(embed)).await?;
     Ok(())
 }
--- src/commands/pp.rs
+++ src/commands/pp.rs
@@ -3,42 +3,36 @@ use poise::{serenity_prelude::{self as serenity, CreateEmbed}, CreateReply};
 use rand_distr::{Distribution, Normal};
 
 /// Measure pp size
-#[poise::command(slash_command, user_cooldown=3, rename="pp", category = "Fun")]
+#[poise::command(slash_command, user_cooldown = 3, rename = "pp", category = "Fun")]
 pub async fn run(
     ctx: Context<'_>,
     #[description = "Person"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
     let user = user.as_ref().unwrap_or_else(|| ctx.author());
-    
-    let pp_size: f64 = 
-        if CONFIG.rig_ids.contains(&user.id.get()) {
-            Normal::new(100000000.0, 10000000.0).unwrap()
-                .sample(&mut rand::thread_rng())
-        } else {
-            Normal::new(12.0, 8.0).unwrap()
-                .sample(&mut rand::thread_rng())
-        };
-        
+
+    let pp_size: f64 = if CONFIG.rig_ids.contains(&user.id.get()) {
+        Normal::new(100000000.0, 10000000.0)
+            .unwrap()
+            .sample(&mut rand::thread_rng())
+    } else {
+        Normal::new(12.0, 8.0)
+            .unwrap()
+            .sample(&mut rand::thread_rng())
+    };
+
     //PP size can go over 24 but it will just cause the colour to be brighter untill every number is 255 or over it
     //which will just convert it to u8 and max it to 255
-    let colour_thingy: f64 = pp_size/22.0; 
+    let colour_thingy: f64 = pp_size / 22.0;
     let colour = serenity::Colour::from_rgb(
-        (196.0*colour_thingy) as u8,
-        (167.0*colour_thingy) as u8, 
-        (231.0*colour_thingy) as u8
+        (196.0 * colour_thingy) as u8,
+        (167.0 * colour_thingy) as u8,
+        (231.0 * colour_thingy) as u8,
     );
 
     let embed = CreateEmbed::default()
-        .description(format!(
-        "{} pp size is {:.2}cm / {:.2}inch",
-        user,
-        pp_size,
-        pp_size * 0.3937007874
-    ))
+        .description(format!("{} pp size is {:.2}cm / {:.2}inch", user, pp_size, pp_size * 0.3937007874))
         .colour(colour);
-     
-    ctx.send(
-        CreateReply{embeds: vec![embed], ..Default::default()}
-    ).await?;
+
+    ctx.send(CreateReply::default().embed(embed)).await?;
     Ok(())
 }
